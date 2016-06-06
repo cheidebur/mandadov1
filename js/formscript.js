0d25c00f115747f4552ed69f40b6c61783f70533
cheidebur <spekter.x@gmail.com>
2016-06-05 18:40:57 -0700

    added clear button functionality, changed color schem

--- js/formscript.js
+++ js/formscript.js
@@ -1,27 +1,46 @@
 $(document).ready(
+
+/*clear button function fail, why does placement at top
+not function?
+
+ $("#clear").click(function() {
+      $(".entries-list").empty();
+      return false;
+      });
+*/
+
+  
+//adding input text value and submit buttom
+
     function(){
-        $('#button').click(
-            function(){
+        $('#button').click(function(){
                 var toAdd = $('input[name=ListItem]').val();
                  $('.entries-list').append('<li>' + toAdd + '</li>');
+
             });
-       
+//using enter key as well as clicking on submit button
+
        $("input[name=ListItem]").keypress(function(event){
           if(event.keyCode == 13) {
             $("#button").click();
             return false;
           }         
       });
-      
-      $(document).on('dblclick','li', function(){
-        $(this).toggleClass('.checked-item').fadeOut('slow');    
+
+//clear button, placement down here works
+
+      $("#clear").click(function(yas) {
+      $(".entries-list").empty();
+      return false;
+      });
+
+
+//double click remove single list item   
+
+      $("ul").on('dblclick','li', function(){
+        $(this).slideUp('slow');    
       });
       
-      $('input').focus(function() {
-        $(this).val('');
-      });
       
-      $('.entries-list > li').sortable();  
       
-    }
-);
+    });
